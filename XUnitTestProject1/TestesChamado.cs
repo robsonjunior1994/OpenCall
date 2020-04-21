3f994ddbd74c5e59e005dfa78f37f2eeb5703f0b
robsonjunior1994 <robsonjunior1994@gmail.com>
2020-04-21 11:35:30 -0300

    Começando a configurar os testes

---
+++ XUnitTestProject1/TestesChamado.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using OpenCall.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class TestesChamado : UnitTest1
+    {
+        [Fact]
+        public void DeveRetornarUmaListaComChamados()
+        {
+            //arrange
+            CriarMock();
+
+
+            //assert
+            chamadoRepositoryMock.Setup(c => c.Get(It.IsAny<IList<Chamado>>))).Returns(Chamados);
+
+            //act
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Moq;
+using OpenCall.Controllers;
+using OpenCall.Models;
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        internal ChamadoController sutChamado;
+        internal Mock<IChamadoRepository> chamadoRepositoryMock;
+
+        public void CriarChamadoController()
+        {
+            sutChamado = new ChamadoController(chamadoRepositoryMock.Object);
+        }
+
+        public void CriarMock()
+        {
+            this.chamadoRepositoryMock = new Mock<IChamadoRepository>();
+        }
+    }
+}
