a23874af5d7efbd979c9adc94ad0bbaead9c6037
robsonjunior1994 <robsonjunior1994@gmail.com>
2020-05-04 11:36:51 -0300

    Subindo Terceiro o segundo teste

--- XUnitTestProject1/TestesChamado.cs
+++ XUnitTestProject1/TestesChamado.cs
@@ -95,5 +95,50 @@ namespace XUnitTestProject1
             //assert
             Assert.True(retorno == null);
             }
+
+        [Fact]
+        public void DeveriaRetornarUmChamadoDeExemploQuandoNaoExistirDadosNoBanco()
+        {
+            //arrange
+            CriarMock();
+            CriarChamadoController();
+
+            IList<Chamado> chamados = new List<Chamado>();
+
+            //var chamado1 = new Chamado()
+            //{
+            //    Id = 01,
+            //    Protocolo = "202004211705",
+            //    Tipo = "água",
+            //    Endereco = "Rua de exemplo",
+            //    Descricao = "Descrição do chamado",
+            //    Status = "Aberto",
+            //    Data = DateTime.Now
+            //};
+
+            //var chamado2 = new Chamado()
+            //{
+            //    Id = 02,
+            //    Protocolo = "202004211706",
+            //    Tipo = "esgoto",
+            //    Endereco = "Rua de exemplo 2",
+            //    Descricao = "Descrição do chamado 2",
+            //    Status = "Aberto",
+            //    Data = DateTime.Now
+            //};
+
+            //chamados.Add(chamado1);
+            //chamados.Add(chamado2);
+
+            //act
+            chamadoRepositoryMock.Setup(x => x.Listar()).Returns(chamados);
+            var retorno = sutChamado.Get("") as OkObjectResult;
+            var teste = retorno.Value as List<Chamado>;
+
+            //assert
+            Assert.True(teste.Count == 1);
         }
+
+    }
+
 }
