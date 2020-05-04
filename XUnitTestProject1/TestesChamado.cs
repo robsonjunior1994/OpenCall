f9c4a704e7b219b574b06c11aa6781164cccd30d
robsonjunior1994 <robsonjunior1994@gmail.com>
2020-05-04 10:12:53 -0300

    Subindo o segundo teste

--- XUnitTestProject1/TestesChamado.cs
+++ XUnitTestProject1/TestesChamado.cs
@@ -53,9 +53,47 @@ namespace XUnitTestProject1
             Assert.True(teste.Count == 2);
         }
 
-        public void PassandoUmStatusQueNaoExisteDeveRetornarUmaListaComTodosOsChamados()
+        [Fact]
+        public void DeverarFalharPosFoiPassadoUmParametrosDiferentesDoEsperadoParaAListagem()
         {
+            //arrange
+                CriarMock();
+            CriarChamadoController();
 
+            IList<Chamado> chamados = new List<Chamado>();
+
+            var chamado1 = new Chamado()
+            {
+                Id = 01,
+                Protocolo = "202004211705",
+                Tipo = "água",
+                Endereco = "Rua de exemplo",
+                Descricao = "Descrição do chamado",
+                Status = "Aberto",
+                Data = DateTime.Now
+            };
+
+            var chamado2 = new Chamado()
+            {
+                Id = 02,
+                Protocolo = "202004211706",
+                Tipo = "esgoto",
+                Endereco = "Rua de exemplo 2",
+                Descricao = "Descrição do chamado 2",
+                Status = "Aberto",
+                Data = DateTime.Now
+            };
+
+            chamados.Add(chamado1);
+                chamados.Add(chamado2);
+ 
+            //act
+            chamadoRepositoryMock.Setup(x => x.Listar()).Returns(chamados);
+            var retorno = sutChamado.Get("open") as OkObjectResult;
+            //var teste = retorno.Value as List<Chamado>;
+          
+            //assert
+            Assert.True(retorno == null);
+            }
         }
-    }
 }
